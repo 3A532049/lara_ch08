c5f8255fa259e4d8d33709dccf552a40d1e9e123
3a532049 <shelly213334@gmail.com>
2018-11-02 23:31:25 +0800

    練習10-1：產生Cool\TestController,新增路由'cool'

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 Route::get('/','HomeController@index');
-
+Route::get('cool','Cool\TestController@index');
 
 Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix'=>'student'], function () {
